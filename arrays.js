e10313eb4dec933a04985fdd1e96cb895be4211b
Ting Chung <tchung682@gmail.com>
2020-05-11 19:56:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,6 @@ var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 var addElementToBeginningOfArray = function(array, element){
   array = [element, ...array]
 }
+var destructivelyAddElementToEndOfArray = function(array, element){
+  array.unshift(element);
+}
