48445e6ee0b93d92794b7b6a1922a16cdc327002
Ting Chung <tchung682@gmail.com>
2020-05-11 20:09:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ var destructivelyAddElementToEndOfArray = function(array, element){
   return array.push(element);
 }
 var destructivelyAddElementToBeginningOfArray = function(array, element){
-  return array.unshift(element)
+  return array.shift(element)
 }
 var addElementToEndOfArray = function(array, element){
   return array = [...array, element]
